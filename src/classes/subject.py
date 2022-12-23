1b4c44cae80a44ee9146cf99648d8f2895a3fbe2
21chanas3 <adenchan@gmail.com>
2022-12-23 15:19:00 -0800

    added constructors

---
+++ src/classes/subject.py
@@ -0,0 +1,4 @@
+class Subject:
+    def __init__(self, name, color=""):
+        self.name = name
+        self.color = color
---
+++ src/classes/tag.py
@@ -0,0 +1,4 @@
+class Tag:
+    def __init__(self, name, color=""):
+        self.name = name
+        self.color = color
