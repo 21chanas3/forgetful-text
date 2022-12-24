2c9dd66812710fef87814d405a71a3d8633d9066
21chanas3 <adenchan@gmail.com>
2022-12-24 14:10:50 -0800

    made the parsing run when main runs

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 import database as db
+import argparse
 import os
 
 os.system('title Forgetful')
 db.create_tables()
+print("Welcome to Forgetful")
 
-input("Press Enter to continue...")
+while True:
+    text = input("> ")
+    if text == "exit":
+        break
+    else:
+        argparse.parser(text)
+
+
+
