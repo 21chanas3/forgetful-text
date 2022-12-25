e5a65f847c5f2af801cc7302691d534cc7321524
21chanas3 <adenchan@gmail.com>
2022-12-25 00:33:57 -0800

    added basic view commands

--- src/commands/view.py
+++ src/commands/view.py
@@ -1,2 +1,19 @@
+from models import Task
+from prettytable import PrettyTable
 def run(args):
-    pass
+    table = PrettyTable()
+    if args[0] == "duedate":
+        table.field_names = ["ID", "Due Date", "Task Name", "Priority", "Do Date"]
+        for task in Task.select().order_by(Task.due_date):
+            table.add_row([task, task.due_date, task.title, task.priority, task.do_date])
+        print(table)
+    elif args[0] == "dodate":
+        table.field_names = ["ID", "Do Date", "Task Name", "Priority", "Due Date"]
+        for task in Task.select().order_by(Task.do_date):
+            table.add_row([task, task.do_date, task.title, task.priority, task.due_date])
+        print(table)
+    elif args[0] == "priority":
+        table.field_names = ["ID", "Do Date", "Task Name", "Priority", "Due Date"]
+        for task in Task.select().order_by(Task.priority):
+            table.add_row([task, task.do_date, task.title, task.priority, task.due_date])
+        print(table)
