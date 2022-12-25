cd88fd4e6d51db06ca95534c2e4439824e62a7fc
21chanas3 <adenchan@gmail.com>
2022-12-25 01:17:32 -0800

    added a today view

--- src/commands/view.py
+++ src/commands/view.py
@@ -1,5 +1,9 @@
 from models import Task
 from prettytable import PrettyTable
+from datetime import datetime, timedelta
+from peewee import fn
+
+
 def run(args):
     table = PrettyTable()
     if args[0] == "duedate":
@@ -15,5 +19,13 @@ def run(args):
     elif args[0] == "priority":
         table.field_names = ["ID", "Do Date", "Task Name", "Priority", "Due Date"]
         for task in Task.select().order_by(Task.priority):
+            table.add_row([task, task.do_date, task.title, task.priority, task.due_date])
+        print(table)
+    elif args[0] == "today":
+        table.field_names = ["ID", "Do Date", "Task Name", "Priority", "Due Date"]
+        for task in Task.select().\
+                where((Task.do_date >= datetime.today().date()) &
+                      (Task.do_date < datetime.today().date() + timedelta(days=1))).\
+                order_by(Task.do_date):
             table.add_row([task, task.do_date, task.title, task.priority, task.due_date])
         print(table)
